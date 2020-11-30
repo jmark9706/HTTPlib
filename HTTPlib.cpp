c1725001bc339d26e80b0967b21e6cc7769714d8
jmark9706 <1157024+jmark9706@users.noreply.github.com>
2020-11-29 21:28:42 -0600

    add class stuff

--- HTTPlib.cpp
+++ HTTPlib.cpp
@@ -1,2 +1,13 @@
 // HTTPlib - helpers
 #include "HTTPlib.h"
+
+HTTPhelper::HTTPhelper()
+{
+};
+
+
+void HTTPhelper::kvstart(WiFiClient client)
+{
+	client.print("999");
+  client.print(this->httpRequest2);
+};
