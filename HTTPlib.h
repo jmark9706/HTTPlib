0f156a9accfc771f88f470157964f26dc61d5169
jmark9706 <1157024+jmark9706@users.noreply.github.com>
2020-11-29 21:29:12 -0600

    class

--- HTTPlib.h
+++ HTTPlib.h
@@ -1,12 +1,18 @@
 // HTTP library
 #ifndef HTTPlib
 #define HTTPlib
+#include <ESP8266WiFi.h>
 
-class HTTPhelpers
+class HTTPhelper
 {public:
-	void HTTPhelper();
-	
-	
+	HTTPhelper();
+	void kvstart(WiFiClient client);
+private:
+const String httpRequest2 = "GET /parms?temp=";
+const String httpRequest3 = "&humidity=";
+const String httpRequest4 = "\nHTTP/1.1\n"
+                           "Host: 10.0.0.151\n"
+                           "Connection: close\n\n";
 };
 
 
